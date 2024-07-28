469dcdb7bbcdf1baaf0d619a0fcdcda2d0344791
PyhtonDeveloperNishu <125293866+PyhtonDeveloperNishu@users.noreply.github.com>
2024-07-29 03:42:57 +0530

    first commit

---
+++ solutionDom/script.js
@@ -0,0 +1,8 @@
+let target = document.getElementById('target');
+target.style.backgroundColor ='yellow';
+target.style.margin ='20px';
+target.style.padding = '10px';
+target.style.fontSize='18px';
+target.style.fontWeight='bold';
+target.style.height='200px';
+target.style.width='300px';
